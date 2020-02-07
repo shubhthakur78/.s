0b94fd5706f4ab2d64ce6fa582d19d44c984a5ec
Shubham Singh Rajput <60695593+shubhthakur78@users.noreply.github.com>
2020-02-07 15:06:36 +0530

    Add files via upload
    pyautogui

---
+++ py_auto_gui_22.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+time.sleep(2)
+print("start")
+data='space'
+time.sleep(2)
+while True:
+    if "space" in data:
+        print("space")
+    pyautogui.keyDown('space')
+    time.sleep(4 )
+    pyautogui.press('space')
+print("done")
+
